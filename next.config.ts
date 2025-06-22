6f03d06ddf040ecab6369bb72b8d4e062c082ccc
ABBEY AINA <olamilekansunday445@gmail.com>
2025-06-22 13:38:03 +0000

    someone told me how they fixed the display layout of the app "Here’s a b

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  serverComponentsExternalPackages: ['mongodb', 'bcryptjs'],
+  webpack: (config) => {
+    // The `handlebars` library, used by Genkit, has a dynamic require that causes a harmless webpack warning.
+    // This rule suppresses that warning.
+    config.ignoreWarnings = [
+      (warning) =>
+        warning.module?.resource?.includes('node_modules/handlebars/'),
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
