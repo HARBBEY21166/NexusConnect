09ba3e07aa5f9785e87e7120df343f7769ff75e7
ABBEY AINA <olamilekansunday445@gmail.com>
2025-06-23 15:55:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,12 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'i.pinimg.com',
-        port: '',
-        pathname: '/**',
+        hostname: 'placehold.co',
       },
     ],
-  },
-  serverComponentsExternalPackages: ['mongodb', 'bcryptjs'],
-  webpack: (config) => {
-    // The `handlebars` library, used by Genkit, has a dynamic require that causes a harmless webpack warning.
-    // This rule suppresses that warning.
-    config.ignoreWarnings = [
-      (warning) =>
-        warning.module?.resource?.includes('node_modules/handlebars/'),
-    ];
-    return config;
   },
 };
 
